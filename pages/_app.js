8056a3e05fd94b12e23b65ea7b685f461f4451a5
gracius yengo <yengogracius@gmail.com>
2023-01-09 14:23:45 +0100

    feat: creat the components(navbar,footer,layout)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Layout from '../public/components/Layout'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+    <Component {...pageProps}
+   />
+      </Layout>
+  )
+  
+  
 }
