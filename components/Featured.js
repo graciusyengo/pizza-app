d1745af76f0aa1313918f724ca21e2271cf5a940
gracius yengo <yengogracius@gmail.com>
2023-01-23 19:29:07 +0100

    refactor:delete icon next and previouly

--- components/Featured.js
+++ components/Featured.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import Image from 'next/image'
-
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/Featured.module.css";
 export default function Featured() {
+  const images = [
+    "/img/featured.png",
+    "/img/featured1.png",
+    "/img/featured2.png",
+  ];
   return (
-    <div>
-      feat
+    <div className={styles["container"]}>
+      <div className={styles.wrapper}>
+        <div className={styles.imageContainer}>
+          {images.map((image, index) => (
+            <Image
+              className="mainImage"
+              src={image}
+              alt=""
+              objectPosition="center"
+              objectFit="cover"
+              key={index}
+             width={800}
+             height={800}
+              
+            />
+          ))}
+         
+          <h2 className={styles.title}>kdkdk</h2>
+        </div>
+      </div>
+      
     </div>
-  )
+  );
 }
