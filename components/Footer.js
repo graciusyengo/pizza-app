2954ed895db35d167fa497ff5c16364ff3237b87
gracius yengo <yengogracius@gmail.com>
2023-02-02 13:37:35 +0100

    feat : create the product and [id].js file

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import styles from "../styles/Footer.module.css";
+
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/featured.png" alt="" width={600} height={600} />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>OH YES, WE DID. GY PIZZA, WELL BAKE SLICE OF PIZZA</h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}> FIND OUR RESTAURANTS </h1>
+          <p className={styles.text}>
+            70 BIS. COLONEL #304,
+            <br /> Kinshasa, 45644
+            <br /> +243972479631
+          </p>
+          <p className={styles.text}>
+            70 BIS. COLONEL #304,
+            <br /> Kinshasa, 45644
+            <br /> +243972479631
+          </p>
+          <p className={styles.text}>
+            70 BIS. COLONEL #304,
+            <br /> Kinshasa, 45644
+            <br /> +243972479631
+          </p>
+          <p className={styles.text}>
+            70 BIS. COLONEL #304,
+            <br /> Kinshasa, 45644
+            <br /> +243972479631
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}> WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 - 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDY - SUNDAY
+            <br /> 12:00 - 22:00
+          </p>
+        </div>
+      </div>
       footer
     </div>
-  )
+  );
 }
--- components/PizzaCard.js
+++ components/PizzaCard.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function PizzaCard() {
   return (
     <div className={styles.container}>
-      <Image src="/img/featured2.png" alt="" width="500" height="500" />
+      <Image className={styles.imgPizza} src="/img/featured2.png" alt="" width="300" height="300"  />
       <h1 className={styles.title}> FIORI DI ZUCCA</h1>
       <span className={styles.price}> $19.00</span>
       <p className={styles.desc}>
